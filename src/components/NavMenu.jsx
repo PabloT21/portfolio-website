2f477fdaa9e5b00fe8d8f0b58f6904d400370326
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2023-01-18 21:13:22 -0300

    Mejoras visuales mínimas

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -5,6 +5,7 @@ import MailIcon from '@mui/icons-material/Mail';
 import WorkIcon from '@mui/icons-material/Work';
 import SchoolIcon from '@mui/icons-material/School';
 import PersonIcon from '@mui/icons-material/Person';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default class NavMenu extends Component {
   render() {
@@ -13,22 +14,22 @@ export default class NavMenu extends Component {
         
         <div className='navMenu-item'>
             <PersonIcon color="primary" fontSize="large"></PersonIcon>
-            <h1>Contact</h1>
+            <h1 className='navMenu-text'>Contact</h1>
         </div>
 
         <div className='navMenu-item'>
             <WorkIcon color="primary" fontSize="large"></WorkIcon>
-            <h1>Contact</h1>
+            <h1 className='navMenu-text'>Experiencia</h1>
         </div>
 
         <div className='navMenu-item'>
             <SchoolIcon color="primary" fontSize="large"></SchoolIcon>
-            <h1>Contact</h1>
+            <h1 className='navMenu-text'>Estudios</h1>
         </div>
 
         <div className='navMenu-item'>
             <MailIcon color="primary" fontSize="large"></MailIcon>
-            <h1>Contact</h1>
+            <h1 className='navMenu-text'>Contacto</h1>
         </div>
 
 
